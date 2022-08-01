e792d99306a01106938f9b098253dd8bc52644cd
tingting <1455809650@qq.com>
2022-08-01 18:16:06 +0800

    文档提交

---
+++ doc/api.md
@@ -0,0 +1,18 @@
+### 其他属性参考antd modal的props
+|属性名|说明|类型|默认值|
+|  ---  | ---  | --- | --- |
+|  title  | 模态框标题 | string | - |
+|  size  | 选择数量 | boolean | 1 |
+|  defaultValue  | 默认选中项 | array | - |
+|  onChange  | 选中触发事件 | function(value) | - |
+
+
+### IndustrySelect.createIndustrySelect
+参数同上
+
+### IndustrySelect.DisplayIndustry
+#### 这是一个组件
+|属性名|说明|类型|默认值|
+|  ---  | ---  | --- | --- |
+|  id  | 选中的项的id | string | - |
+
---
+++ example/src/readme.js
@@ -0,0 +1,101 @@
+import * as component_81 from '@kne/react-industry-select';
+import * as component_82 from 'antd';
+const readmeConfig = {
+    name: `@kne/react-industry-select`,
+    description: `行业选择器`,
+    summary: `<p>全国城市选择器</p>`,
+    api: `<h3>其他属性参考antd modal的props</h3>
+<table>
+<thead>
+<tr>
+<th>属性名</th>
+<th>说明</th>
+<th>类型</th>
+<th>默认值</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>title</td>
+<td>模态框标题</td>
+<td>string</td>
+<td>-</td>
+</tr>
+<tr>
+<td>size</td>
+<td>选择数量</td>
+<td>boolean</td>
+<td>1</td>
+</tr>
+<tr>
+<td>defaultValue</td>
+<td>默认选中项</td>
+<td>array</td>
+<td>-</td>
+</tr>
+<tr>
+<td>onChange</td>
+<td>选中触发事件</td>
+<td>function(value)</td>
+<td>-</td>
+</tr>
+</tbody>
+</table>
+<h3>IndustrySelect.createIndustrySelect</h3>
+<p>参数同上</p>
+<h3>IndustrySelect.DisplayIndustry</h3>
+<h4>这是一个组件</h4>
+<table>
+<thead>
+<tr>
+<th>属性名</th>
+<th>说明</th>
+<th>类型</th>
+<th>默认值</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>id</td>
+<td>选中的项的id</td>
+<td>string</td>
+<td>-</td>
+</tr>
+</tbody>
+</table>`,
+    example: {
+        isFull: false,
+        className: `react_industry_select_8c848`,
+        style: ``,
+        list: [{
+    title: `行业类别`,
+    description: `选择行业类别，可支持多选，数据可以自己扩展`,
+    code: `const {createIndustrySelect,DisplayIndustry}=IndustrySelect;
+const {Button}=Antd;
+
+const BaseExample = ()=>{
+    return <Button onClick={()=>{
+        createIndustrySelect({
+            defaultValue:["040"]
+        })
+    }}>
+        <DisplayIndustry id={"040"}>{(list)=>list.chName}</DisplayIndustry>
+    </Button>
+};
+
+render(<BaseExample />);
+
+`,
+    scope: [{
+    name: "IndustrySelect",
+    packageName: "@kne/react-industry-select",
+    component: component_81
+},{
+    name: "Antd",
+    packageName: "antd",
+    component: component_82
+}]
+}]
+    }
+};
+export default readmeConfig;
